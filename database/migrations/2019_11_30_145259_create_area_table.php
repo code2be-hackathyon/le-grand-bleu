8c752eb9af27fa7f2dab21c662bec7f75119d6e4
Willy-Gilly <willygilly85@gmail.com>
2019-11-30 17:18:00 +0100

    Migration DB

---
+++ database/migrations/2019_11_30_145259_create_area_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('area', function (Blueprint $table) {
+            $table->bigIncrements('area_id');
+            $table->string('area_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('area');
+    }
+}
