33f47afdc8b996c3255a8ce9ce65faf3ffd4ee2b
olimalt <olimalt85@live.fr>
2019-12-03 10:41:44 +0100

    add an exemple of data insert into database in the job

--- app/Jobs/Test.php
+++ app/Jobs/Test.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
 
 class Test implements ShouldQueue
 {
@@ -29,6 +31,7 @@ class Test implements ShouldQueue
      */
     public function handle()
     {
+        DB::insert('insert into testolimalt (test1, test2) values (2, 3)'); //in the testolimalt table  add values 2 and 3 in test1 and test2
         $mavaleur="test";
         var_dump($mavaleur);
     }
